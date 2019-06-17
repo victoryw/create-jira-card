eb9fa15a3935e5e44486e1302220b128eadc88db
tianyawy <tianya_wy@hotmail.com>
2019-06-17 22:44:25 +0800

    export to csv change the read data from data.js to read data from the file which input by command

--- services/load-method-dependency-service.js
+++ services/load-method-dependency-service.js
@@ -1,5 +1,18 @@
 const axios = require('axios');
 const { DepGraph } = require('dependency-graph');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const datas = require('../services/data');
+
+const csvWriter = createCsvWriter({
+  path: '/Users/victoryw/myfiles/TW-Projects/picc/create-card-factory/file.csv',
+  header: [
+    { id: 'class', title: 'class' },
+    { id: 'method', title: 'method' },
+    { id: 'caller', title: 'caller' },
+    { id: 'view', title: 'view' },
+  ],
+});
+
 
 const loadDependencyProvider = async (_url) => {
   const dependencyClient = axios.create();
@@ -13,29 +26,43 @@ const loadDependencyProvider = async (_url) => {
 
 const loadUri = async (_url, _url2, className, method) => {
   const requestUrl = `${_url}/method/${className}/${method}/invokes?deep=20`;
-  const result = await loadDependencyProvider(requestUrl);
   const depGraph2 = new DepGraph();
-  result.nodes.forEach((node) => {
-    depGraph2.addNode(node.id);
-    depGraph2.setNodeData(node.id, node);
-  });
-  result.edges.forEach((edge) => {
-    depGraph2.addDependency(edge.b, edge.a);
-  });
-  result.nodes.filter(node => node.title.indexOf('com.ebao.life.servlet.pub.common.RootServlet') > -1).forEach(node => depGraph2.removeNode(node.id));
-  const graph2s = depGraph2.overallOrder(true);
-  const callerNames = graph2s.map(root => depGraph2.getNodeData(root).title);
-  return {
-    caller: callerNames,
-    view: `${_url2}/method/${className}/${method}/invokes?deep=20`,
-    class: className,
-    method,
-  };
+
+  try {
+    const result = await loadDependencyProvider(requestUrl);
+    result.nodes.forEach((node) => {
+      depGraph2.addNode(node.id);
+      depGraph2.setNodeData(node.id, node);
+    });
+    result.edges.forEach((edge) => {
+      depGraph2.addDependency(edge.b, edge.a);
+    });
+    result.nodes.filter(node => node.title.indexOf('com.ebao.life.servlet.pub.common.RootServlet') > -1
+                        || node.title.indexOf('com.ebao.pub.framework.GenericAction') > -1
+                        || node.title.indexOf('com.picc.jmstask.JmsTask') > -1).forEach(node => depGraph2.removeNode(node.id));
+
+    const graph2s = depGraph2.overallOrder(true);
+    const callerNames = graph2s.map(root => depGraph2.getNodeData(root).title);
+    return {
+      caller: callerNames,
+      view: `${_url2}/method/${className}/${method}/invokes?deep=20`,
+      class: className,
+      method,
+    };
+  } catch (error) {
+    console.log(error, `${_url2}/method/${className}/${method}/invokes?deep=20`);
+    return {
+      caller: 'error',
+      view: `${_url2}/method/${className}/${method}/invokes?deep=20`,
+      class: className,
+      method,
+    };
+  }
 };
 
 const loads = async (_url, _url2, todos) => {
   const results = await Promise.all(todos.map(async (todo) => {
-    const result2 = await loadUri(_url, _url2, todo.className, todo.method);
+    const result2 = await loadUri(_url, _url2, todo[0], todo[1]);
     return result2;
   }));
   return results;
@@ -43,10 +70,11 @@ const loads = async (_url, _url2, todos) => {
 
 const loadDependency = (_url, _url2) => ({
   load: async () => {
-    const className = 'com.ebao.life.claim.claimentry.ClaimBackEntryDAO';
-    const method = 'saveUwBackToEntry';
-    const todos = [{ className, method }];
-    console.log(await loads(_url, _url2, todos));
+    const totalResults = await Promise.all(datas.map(async (data) => {
+      const result = await loads(_url, _url2, [data]);
+      return result;
+    }));
+    await csvWriter.writeRecords(totalResults.flatMap(x => x));
   },
 });
 
